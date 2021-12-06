ef73cecc5d2cf3d451787ce7d9142a5db25031aa
Murtaza Vohra <xvyurei@gmail.com>
2021-12-06 14:15:31 +0000

    Setup php functions

---
+++ html/signup.php
@@ -0,0 +1,30 @@
+<?php
+ include('../php/common.php');
+
+ echo
+ html(
+    head(
+        title('Sign Up') .
+        stylesheet('../css/common.css') .
+        script('../js/common.js') .
+        script('../js/accounts.js') .
+        script('../js/validation.js') .
+        script('../js/signup.js')
+        ) .
+    body(
+        array(),
+        nav() .
+        main(
+            container(
+                logo() .
+                card('Sign Up',
+                    container(
+                        field('name', 'Name') .
+                        field('email', 'Email') .
+                        field('phone', 'Phone') .
+                        field('password', 'Password')
+                        ) .
+                        error() .
+                        containerRight(
+                        button('Sign Up', 'handleSignUp();')))))));
+ ?>
--- php/common.php
+++ php/common.php
@@ -1,43 +1,13 @@
-<?php
 
-function doc($title, $links, $includeHeader, $content) {
-    $linksString = '';
+ <?php
 
-    foreach ($links as $link) {
-        if ($link[0] == 'css') {
-            $linksString = $linksString . "<link href=\" $link[1] \" rel=\"stylesheet\">";
-        } elseif ($link[0] == 'script') {
-            $linksString = $linksString . "<script src=\" $link[1] \"></script>";
-        }
-    }
+function nav() {
+    $links = array(
+        array("Login", "./index.php"),
+        array("Sign Up", "./signup.php"),
+        array("Rankings", "./rankings.php")
+    );
 
-    $header = "";
-    if ($includeHeader) {
-        $header = nav(array(
-            array("Login", "./index.html"),
-            array("Sign Up", "./signup.html"),
-            array("Rankings", "./rankings.html")
-        ));
-    }
-
-    return "
-<!DOCTYPE html>
-
-<html>
-    <head>
-        <title> $title </title>
-        $linksString
-    </head>
-
-    <body>
-        $header
-        <main> $content </main>
-    </body>
-</html>
-";
-}
-
-function nav($links) {
     $linksString = '';
 
     foreach ($links as $link) {
@@ -47,23 +17,124 @@ function nav($links) {
         $linksString = $linksString . "<a class=\"nav-item\" href=\"$href\"> $title </a>";
     }
 
-    return "
-        <header class=\"nav\"> $linksString </header>
-";
+    return tag('header', array('class', 'nav'), $linksString);
 }
 
-function page($title, $links, $content) {
+function container($content) {
+    return div(array('class', 'container'), $content);
+}
 
+function containerRight($content) {
+    return div(array('class', 'container-right'), $content);
+}
+
+function logo() {
+    return div(array('class', 'logo'), "Chess");
+}
+
+function card($title, $content) {
+    return div(array('class', 'card'),
+               cardTitle($title) .
+               $content);
+}
+
+function cardTitle($title) {
+    return container(div(array('class', 'card-title'), $title));
+}
+
+function sectionTitle($title) {
+    return  div(array('class', 'section-title'), $title);
+}
+
+function space() {
+    return div(array('class', 'vert-space-24'), "");
+}
+
+function field($name, $label, $type='text') {
+    return div(array('class', 'field-container'),
+        tag('label', array('class', 'field-label', 'for', $name), $label) .
+    "<input class=\"field\" type=\"$type\" id=\"$name\" name=\"$name\" placeholder=\"$label\" />");
+}
+
+function error() {
+    return div(array(
+        'id', 'error',
+        'class', 'error-text',
+        'hidden', 'true'), '');
+}
+
+function table($id, $cols) {
+    $headRow = "";
+
+    foreach ($cols as $col) {
+        $headRow = $headRow . tag('th', array(), $col);
+    }
+
+    return div(array('class', 'table-container'),
+            tag('table', array('id', $id),
+                tag('thead', array(),
+                    tag('tr', array(), $headRow)) .
+                tag('tbody', array(), "")));
+}
+
+function div($extraAttrs, $content) {
+    return tag('div', $extraAttrs, $content);
+}
+
+function main($content) {
+    return tag('main', array(), $content);
+}
+
+function button($title, $onClick) {
+    return tag('button',
+               array(
+                   'class', 'button',
+                   'onclick', $onClick
+               ),
+               $title);
+}
+
+function html($content) {
+    return
+        "<!DOCTYPE html>" .
+        tag('html', array(), $content);
+}
+
+function head($content) {
+    return tag('head', array(), $content);
+}
+
+function title($content) {
+    return tag('title', array(), $content);
+}
+
+function stylesheet($href) {
+    return "<link href='$href' rel='stylesheet'>";
+}
+
+function script($src) {
+    return tag('script', array('src', $src), "");
+}
+
+function body($attrs, $content) {
+    return tag('body', $attrs, $content);
+}
+
+function tag($name, $attrs, $content) {
+    $attrString = attrs($attrs);
+    return "<$name $attrString> $content </$name>";
 }
 
 function attrs($attributes) {
     $string = '';
 
-    foreach ($attributes as $attribute) {
-        $name = $attribute[0];
-        $value = $attribute[1];
+    $attrCount = count($attributes);
+    for ($x = 0; $x < $attrCount; $x += 2) {
 
-        $string = $string . "$name = \" $value \" ";
+        $name = $attributes[$x];
+        $value = $attributes[$x + 1];
+
+        $string = $string . "$name=\"$value\", ";
     }
 
     return $string;
